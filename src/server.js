c4478ce38f4c5897f24677d98ba4d455f2c7064a
Muhammad Sayuti <sayutizxc@gmail.com>
2021-02-21 23:39:49 +0800

    first commit

---
+++ src/server.js
@@ -0,0 +1,40 @@
+const morgan = require('morgan');
+const express = require('express');
+const app = express();
+const ytDownloader = require('./yt_downloader');
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res, next) => {
+  res.send('Hello World');
+});
+
+app.get('/yt', async (req, res, next) => {
+  try {
+    const data = await ytDownloader(req.query.url, req.query.q);
+    res.json({
+      status: res.statusCode,
+      result: data,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Handle error 404
+app.use((req, res, next) => {
+  err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Send error message
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.send({
+    status: error.status || 500,
+    message: error.message,
+  });
+});
+
+app.listen(3000);
---
+++ src/yt_downloader.js
@@ -0,0 +1,56 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+module.exports = async function ytDownloader(reqUrl, videoQuality) {
+  const youtube = {};
+  const q = ['144', '240', '360', '480', '720', '1080'];
+  const url = `${reqUrl}`;
+
+  // RegExp untuk memvalidasi url
+  const regexUrl = /^((https?:\/\/)?(www.)?(youtu(be)?.(be|com))\/(watch\?v=)?)/;
+
+  // RegExp untuk mengambil nilai dari k__id
+  const regexUniqId = /(?<=k__id.?=.?("|'))\w+/;
+
+  // RegExp untuk mengambil download url dari video
+  const regexDownloadUrl = /(?<=<a href=")[\w:\/\/\.\?\=]+/;
+
+  // Menentukan kualitas video
+  const quality = q.includes(videoQuality)
+    ? ['144', '240', '720'].includes(videoQuality)
+      ? `${videoQuality}p`
+      : videoQuality
+    : '360';
+
+  // Cek apakah url sudah benar
+  if (!regexUrl.test(url)) throw new Error('Invalid URL');
+  const videoId = url.replace(regexUrl, '').trim();
+  try {
+    const response = await axios({
+      method: 'post',
+      url: 'https://www.y2mate.com/mates/analyze/ajax',
+      data: `url=https://www.youtube.com/watch?v=${videoId}&q_auto=1&ajax=1`,
+    }).then((response) => response.data);
+    const $ = cheerio.load(response.result);
+
+    // Ambil title dari video
+    youtube.title = $('.caption.text-left b').text().trim();
+
+    // Ambil thumbnail dari video
+    youtube.thumb = $('.thumbnail.cover img').attr('src');
+
+    // Cek apakah ada k__id atau tidak
+    if (!regexUniqId.test(response.result))
+      throw new Error('Tidak dapat mendownload video');
+    const uniqId = regexUniqId.exec(response.result)[0];
+    const result = await axios({
+      method: 'post',
+      url: 'https://www.y2mate.com/mates/convert',
+      data: `type=youtube&_id=${uniqId}&v_id=${videoId}&ajax=1&token=&ftype=mp4&fquality=${quality}`,
+    }).then((response) => response.data);
+    youtube.download_url = regexDownloadUrl.exec(result.result)[0];
+    return youtube;
+  } catch (error) {
+    throw new Error(`${error}`);
+  }
+};
